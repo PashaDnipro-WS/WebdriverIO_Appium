c57899fd6b78ee487db4c931e00564fddb3a0d22
Pasha Dnipro <pzadniprovskij@gmail.com>
2026-06-08 12:55:21 +0300

    initial commit

---
+++ config/wdio.conf.android.bs.ts
@@ -0,0 +1,41 @@
+import { config as sharedConfig } from './wdio.conf.js'
+import dotenv from 'dotenv'
+
+dotenv.config()
+
+export const config = {
+  ...sharedConfig,
+
+  user: process.env.BROWSERSTACK_USERNAME,
+  key: process.env.BROWSERSTACK_ACCESS_KEY,
+  hostname: 'hub.browserstack.com',
+
+  services: [
+    ['browserstack', {
+      app: process.env.BROWSERSTACK_ANDROID_APP_ID,
+      testObservability: true,
+      testObservabilityOptions: {
+        projectName: 'BrowserStack Android app testing',
+        buildName: 'browserstack Android build',
+      },
+      debug: true,
+      networkLogs: true,
+      consoleLogs: 'warn'
+    }]
+  ],
+
+  capabilities: [{
+    'bstack:options': {
+      deviceName: 'Samsung Galaxy S22 Ultra',
+      osVersion: '12.0',
+      deviceOrientation: 'portrait',
+      buildName: `Android build ${process.env.GITHUB_RUN_NUMBER || 'local'}`
+    },
+    'appium:options': {
+      automationName: 'UiAutomator2',
+      autoAcceptAlerts: true,
+      autoGrantPermissions: true,
+      newCommandTimeout: 300
+    }
+  }]
+}
---
+++ config/wdio.conf.android.ts
@@ -0,0 +1,29 @@
+import { config as sharedConfig } from './wdio.conf.js'
+import { join } from 'path'
+
+export const config = {
+  ...sharedConfig,
+
+  port: 4723,
+
+  services: ['appium'],
+
+  appium: {
+    command: 'appium',
+    args: [
+      '--relaxed-security',
+      '--session-override'
+    ]
+  },
+
+  capabilities: [{
+    platformName: 'Android',
+    'appium:deviceName': 'RF8R90007TX',
+    'appium:udid': 'RF8R90007TX',
+    'appium:app': join(process.cwd(), './apps/android/mda-2.2.0-25.apk'),
+    'appium:automationName': 'UiAutomator2',
+    'appium:newCommandTimeout': 300,
+    'appium:noReset': false,
+    'appium:autoGrantPermissions': true
+  }]
+}
